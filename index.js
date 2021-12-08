83a52a49abcda2ad2f1f619b1ada188b8e2db73e
Mikita Papok <73660686+mikitapapok@users.noreply.github.com>
2021-12-08 18:00:07 +0300

    add changes according to comments in PR

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const slider=document.querySelector('.slider')
-const buttonNext=document.querySelector('.slider-button-next')
-const buttonPrev=document.querySelector('.slider-button-prev')
-const sheet= document.querySelector('.sheet')
-const sidebar=document.querySelector('.sidebar')
-const burgerMenu=document.querySelector('.burger-menu')
-const sidebarNav=document.querySelectorAll('.sidebar-nav-list__component')
-const sideBarBurgerButton=document.querySelector('.sidebar-button')
-function toggleSlider(e){
-
-        slider.classList.toggle('slider-2')
-        slider.classList.toggle('slider-1')
-
+const slider = document.querySelector('.slider')
+const buttonNext = document.querySelector('.slider-button-next')
+const buttonPrev = document.querySelector('.slider-button-prev')
+const sheet = document.querySelector('.sheet')
+const sidebar = document.querySelector('.sidebar')
+const burgerMenu = document.querySelector('.burger-menu')
+const sidebarNav = document.querySelectorAll('.sidebar-nav-list__component')
+const sideBarBurgerButton = document.querySelector('.sidebar-button')
+function toggleSlider(e) {
+  slider.classList.toggle('slider-2')
+  slider.classList.toggle('slider-1')
 }
-function hideSidebar(){
-        sheet.style.display='none'
-        sidebar.style.display='none'
+function hideSidebar() {
+  sheet.classList.add('burger-disabled')
+  sidebar.classList.add('burger-disabled')
 }
 
-buttonNext.addEventListener('click',toggleSlider)
-buttonPrev.addEventListener('click',toggleSlider)
+buttonNext.addEventListener('click', toggleSlider)
+buttonPrev.addEventListener('click', toggleSlider)
 
-sheet.addEventListener('click',hideSidebar)
+sheet.addEventListener('click', hideSidebar)
 
-burgerMenu.addEventListener('click',()=>{
-        sheet.style.display='block'
-        sidebar.style.display='flex'
+burgerMenu.addEventListener('click', () => {
+  sheet.classList.remove('burger-disabled')
+  sidebar.classList.remove('burger-disabled')
 })
-sidebarNav.forEach(element=>{
-        element.addEventListener('click',hideSidebar)
+sidebarNav.forEach((element) => {
+  element.addEventListener('click', hideSidebar)
 })
-sideBarBurgerButton.addEventListener('click',hideSidebar)
+sideBarBurgerButton.addEventListener('click', hideSidebar)
